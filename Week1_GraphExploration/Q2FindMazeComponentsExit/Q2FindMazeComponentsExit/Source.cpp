ec683e3a1311b3edd27f8e6cf6af532496045b15
Vincent Gim <vgim@sateva.com.au>
2017-02-26 12:58:29 +0800

    Added solution for question 2

--- Week1_GraphExploration/Q2FindMazeComponentsExit/Q2FindMazeComponentsExit/Source.cpp
+++ Week1_GraphExploration/Q2FindMazeComponentsExit/Q2FindMazeComponentsExit/Source.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using std::vector;
 using std::pair;
+using std::unordered_set;
+
+void explore(const vector<vector<int>> &adj, unordered_set<int> &visited, int v) {
+	int size = adj[v].size();
+	visited.insert(v);
+	
+	for (size_t i = 0; i < size; i++) {
+		int neighbour = adj[v][i];
+		if (visited.find(neighbour) == visited.end()) {
+			explore(adj, visited, neighbour);
+		}
+	}	
+};
 
 int number_of_components(vector<vector<int> > &adj) {
+	unordered_set<int> visited;
 	int res = 0;
-	//write your code here
+	
+	for (size_t i = 0; i < adj.size(); i++) {
+		if (visited.find(i) == visited.end()) {			
+			explore(adj, visited, i);
+			res++;
+		}
+	}	
+
 	return res;
 }
 
@@ -15,7 +37,7 @@ int main() {
 	std::cin >> n >> m;
 	vector<vector<int> > adj(n, vector<int>());
 	for (size_t i = 0; i < m; i++) {
-		int x, y;
+		int x, y; 
 		std::cin >> x >> y;
 		adj[x - 1].push_back(y - 1);
 		adj[y - 1].push_back(x - 1);
