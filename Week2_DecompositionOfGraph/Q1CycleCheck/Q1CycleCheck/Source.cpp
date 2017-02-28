0118d23034211502a2e7a63501cf3f3087f8696d
Vincent Gim <vgim@sateva.com.au>
2017-02-28 23:10:24 +0800

    Initial solution

--- Week2_DecompositionOfGraph/Q1CycleCheck/Q1CycleCheck/Source.cpp
+++ Week2_DecompositionOfGraph/Q1CycleCheck/Q1CycleCheck/Source.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
+#include <unordered_set>
 #include <vector>
 
 using std::vector;
 using std::pair;
+using std::unordered_set;
+
+bool explore(const vector<vector<int>> &adj, unordered_set<int> &visited, int v) {
+	int size = adj[v].size();
+	visited.insert(v);
+	bool hasCycle = false;
+
+	if (visited.find(v) == visited.end()) {
+		for (size_t i = 0; i < size; i++) {
+			hasCycle = explore(adj, visited, adj[v][i]);
+		}
+	}
+	else {
+		return true;
+	}
+
+	return hasCycle || false;
+}
 
 int acyclic(vector<vector<int> > &adj) {
-	//write your code here
-	return 0;
+	unordered_set<int> visited;
+
+	bool hasCycle;
+	for (size_t i = 0; i < adj.size(); i++) {
+		hasCycle = explore(adj, visited, i);
+	}
+
+	return hasCycle ? 1 : 0;
 }
 
 int main() {
