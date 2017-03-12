f55f1a69340cf8f6b621b60063d84444a59166b5
Vincent Gim <vgim@sateva.com.au>
2017-03-12 16:49:00 +0800

    Initial bipartite BFS logic

--- Week3_MostDirectRoute/Q2BipartiteGraph/Q2BipartiteGraph/Source.cpp
+++ Week3_MostDirectRoute/Q2BipartiteGraph/Q2BipartiteGraph/Source.cpp
@@ -5,9 +5,35 @@
 using std::vector;
 using std::queue;
 
-int bipartite(vector<vector<int> > &adj) {
-	//write your code here
-	return -1;
+int bipartite(const vector<vector<int>> &adj) {
+	int adjSize = adj.size();
+	vector<int> color(adjSize, -1);
+	queue<int> q;
+
+	q.emplace(0);
+	color[0] = 1;
+
+	bool bipartiteState = 0;	// This will flip for each alternating neighbour
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+
+		for (size_t i = 0; i < adj[u].size(); i++) {
+			int v = adj[u][i];
+			if (color[v] == -1) {
+				q.emplace(adj[u][i]);
+				color[v] = bipartiteState;
+			}
+			else {
+				if (color[v] != bipartiteState) {
+					return 0;
+				}
+			}
+		}
+		bipartiteState = !bipartiteState;
+	}
+
+	return 1;
 }
 
 int main() {
