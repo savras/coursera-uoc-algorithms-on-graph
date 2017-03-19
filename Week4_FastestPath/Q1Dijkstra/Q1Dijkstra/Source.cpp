0352237b8e7d4918aae51103382e23542a898ff1
Vincent Gim <vgim@sateva.com.au>
2017-03-19 19:30:21 +0800

    Initial solution

--- Week4_FastestPath/Q1Dijkstra/Q1Dijkstra/Source.cpp
+++ Week4_FastestPath/Q1Dijkstra/Q1Dijkstra/Source.cpp
@@ -1,22 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <utility>
 
 using std::vector;
 using std::queue;
 using std::pair;
 using std::priority_queue;
+using std::make_pair;
 
-int distance(vector<vector<int> > &adj, vector<vector<int> > &cost, int s, int t) {
-	//write your code her
-	return -1;
+int distance(const vector<vector<int>> &adj, const vector<vector<int>> &cost, const int &s, const int &t) {
+	int size = adj.size();
+	vector<int> dist(size, INT_MAX);
+	vector<int> prev(size, -1);
+	priority_queue<pair<int, int>> q;
+
+	dist[s] = 0;
+	pair<int, int> u;
+
+	pair<int, int> ps = make_pair(s, dist[s]);
+	q.push(ps);	
+
+	while (!q.empty()) {
+		u = q.top();
+		q.pop();
+
+		for (size_t i = 0; i < adj[u.first].size(); i++) {
+			int v = adj[u.first][i];
+			int d = dist[u.first] + cost[u.first][i];
+			if (dist[adj[u.first][i]] > d) {
+				dist[adj[u.first][i]] = d;
+			}
+			q.emplace(make_pair(adj[u.first][i], d));
+		}
+	}	
+
+	return 0;
+	
 }
 
 int main() {
 	int n, m;
 	std::cin >> n >> m;
-	vector<vector<int> > adj(n, vector<int>());
-	vector<vector<int> > cost(n, vector<int>());
+	vector<vector<int>> adj(n, vector<int>());
+	vector<vector<int>> cost(n, vector<int>());
 	for (int i = 0; i < m; i++) {
 		int x, y, w;
 		std::cin >> x >> y >> w;
