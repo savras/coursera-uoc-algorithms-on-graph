5103e0616f0bb811df0a13e5f69aff3c360ba8d5
Vincent Gim <vgim@sateva.com.au>
2017-04-01 17:32:09 +0800

    Initial prim's algorithm

--- Week5_MinimumSpanningTree/Q1BuildRoads/Q1BuildRoads/Source.cpp
+++ Week5_MinimumSpanningTree/Q1BuildRoads/Q1BuildRoads/Source.cpp
@@ -9,29 +9,63 @@
 using std::vector;
 using std::priority_queue;
 using std::pair;
+using std::sqrt;
 using std::make_pair;
 using std::greater;
 
-double build_adj(vector<vector<int>> &adj, const vector<int> &x, const vector<int> &y) {
+double length_of_segments(const int &p1, const int &p2, const vector<int> &x, const vector<int> &y) {
+	int weight = sqrt((x[p1] - x[p2]) + (y[p1] - y[p2]));
+	return weight;
+}
+
+void build_adj(vector<vector<double>> &cost, const vector<int> &x, const vector<int> &y) {
+	int size = x.size();
+	for (size_t i = 0; i < size; i++) {
+		for (size_t p = i + 1; p < size; p++) {
+			double weight = length_of_segments(i, p, x, y);
+			cost[i][p] = cost[p][i] = weight;
+		}
+	}
+}
+
+// Kruskal's
+double minimum_distance_kruskal(vector<int> x, vector<int> y) {
 	return 0.0;
 }
 
-double length_of_segments() {
+double calc_minimum_distance(const vector<int> &prev, const vector<vector<double>> &cost) {
 	return 0.0;
 }
 
-double minimum_distance(vector<int> x, vector<int> y) {
-	vector<vector<int>> adj;
-	build_adj(adj, x, y);
+// Prim's using priority heap
+double minimum_distance_prim(vector<int> x, vector<int> y) {
+	int size = x.size();
+	vector<vector<double>> cost(size, vector<double>(size, 0));
+	build_adj(cost, x, y);	
+	
+	vector<int> prev(size, 0);
+	vector<int> dist(size, INT_MAX);	
+	dist[0] = 0;
+	
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> H;	// Min heap STL.
+	H.push(make_pair(0, 0));
+	
+	while (!H.empty()) {
+		int u = H.top().second;	// vertex index is stored as second value.
+		H.pop();
 
-	double result = 0.;
+		int d = dist[u];
+		for (size_t m = 0; m < size; m++) {
+			int v = m;
+			int c = cost[u][m];
+			if (c < dist[v]) {
+				dist[v] = d;
+				H.push(make_pair(c, v));
+			}
+		}
+	}
 
-	vector<int> dist;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> H;	// Holds nodes NOT in the known region.
-
-
-
-	return result;
+	return calc_minimum_distance(prev, cost);
 }
 
 int main() {
@@ -41,5 +75,5 @@ int main() {
 	for (size_t i = 0; i < n; i++) {
 		std::cin >> x[i] >> y[i];
 	}
-	std::cout << std::setprecision(10) << minimum_distance(x, y) << std::endl;
+	std::cout << std::setprecision(10) << minimum_distance_prim(x, y) << std::endl;
 }
