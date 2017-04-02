53257237a78ca1ce068045f20460bfe2cf78d61f
Vincent Gim <vgim@sateva.com.au>
2017-04-02 20:06:30 +0800

    Added comments

--- Week5_MinimumSpanningTree/Q1BuildRoads/Q1BuildRoads/Source.cpp
+++ Week5_MinimumSpanningTree/Q1BuildRoads/Q1BuildRoads/Source.cpp
@@ -1,3 +1,8 @@
+/*
+ * Note that dist does not give the shortest distance for the MST. It is used in the algorithm
+ * to indicate the shortest edge from a vertex. To get the shorest path (all edge weight) we
+ * need to get the cost for all edges/paths in the parent array.
+ */
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
